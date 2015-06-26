104dbcc46d151aca78a7f37330e689b1590fb115
Michael <michael.shihrer@gmail.com>
2015-06-26 06:11:55 -0600

    Iniitial Commit

---
+++ SharpMedia/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpMedia.Models
+{
+    class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime Year { get; set; }
+        public int Runtime { get; set; }
+        public int MPAARatingID { get; set; }
+
+
+        public virtual MPAARating MPAARating { get; set; }
+        public virtual ICollection<Genre> Genres { get; set; }
+    }
+}
